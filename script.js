7d62d46b0fba09334ac3edda56a48323da2273a1
Yuuta Chiba <dirac0239@gmail.com>
2026-06-23 23:01:50 +0900

    JavaScript作品① Version1.3 入力対応・送料判定・入力チェック追加

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-const price = 300;
-const tax = 0.08;
+const price = Number(prompt("商品の値段を入力して下さい。"));
+const tax = Number(prompt("消費税率を入力してください。(例:8、10)")) / 100;
+
+if (isNaN(price) || isNaN(tax)) {
+  console.log("数字を入力してください。");
+} else {
 const total = price + (price * tax);
 
-console.log("税込価格は" + total.toFixed(2) + "円です。" )
+console.log("税込価格は" + total.toFixed(2) + "円です。" );
+
+if (total>=1000) {
+  console.log("送料無料です。");
+} else if (total>=500) {
+  console.log("送料300円です。");
+} else {
+  console.log("送料500円です。");
+}
+}
