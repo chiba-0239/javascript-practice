11dd5bc84855ab6d9e81757d4ce71dcb97960791
Yuuta Chiba <dirac0239@gmail.com>
2026-06-24 23:13:33 +0900

    JavaScript作品① Version1.4 会員割引機能追加

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const price = Number(prompt("商品の値段を入力して下さい。"));
 const tax = Number(prompt("消費税率を入力してください。(例:8、10)")) / 100;
+const member = prompt("会員ですか？（はい／いいえ）");
 
 if (isNaN(price) || isNaN(tax)) {
   console.log("数字を入力してください。");
 } else {
-const total = price + (price * tax);
+let total = price + (price * tax);
+
+if (member ==="はい") {
+  total = total * 0.95;
+}
 
 console.log("税込価格は" + total.toFixed(2) + "円です。" );
 
